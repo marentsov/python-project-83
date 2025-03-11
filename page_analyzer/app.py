bdccfda1fe59f749bf5ec1aaace4a3ae611160ab
marentsov <mr.marentsov@gmail.com>
2025-03-11 14:04:31 +0300

    add parser and database

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,15 +1,17 @@
 import os
+import requests
 
 from dotenv import load_dotenv
 from flask import Flask, render_template, request
 
+from page_analyzer.parser import get_data
 from page_analyzer.url_validator import normalize_url
 
 load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-# DATABASE_URL = os.getenv('DATABASE_URL')
-# conn = psycopg2.connect(DATABASE_URL)
+DATABASE_URL = os.getenv('DATABASE_URL')
+conn = psycopg2.connect(DATABASE_URL)
 
 
 @app.route('/')
@@ -23,7 +25,11 @@ def index():
 def urls_index():
     user_url = request.form.to_dict()['url']
     url = normalize_url(user_url)
+    url_response = requests.get(url)
+    url_response.raise_for_status()
+    url_data = get_data(url_response)
     return render_template(
         'url.html',
-        url=url
+        url=url,
+        url_data=url_data
     )
--- page_analyzer/parser.py
+++ page_analyzer/parser.py
@@ -1,5 +1,22 @@
+from bs4 import BeautifulSoup
 
 
-#
+def get_data(content):
+    soup = BeautifulSoup(content.text, "lxml")
+    data = {}
+
+    h1_tag = soup.h1.string if soup.h1 else None
+    title_tag = soup.title.string if soup.title else None
+    description_tag = soup.find('meta', {'name': 'description'}).get('content')
+
+    data['h1'] = h1_tag
+    data['title'] = title_tag
+    data['description'] = description_tag if description_tag else None
+
+    return data
+
+
+
+
 
 
