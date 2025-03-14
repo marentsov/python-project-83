fa46cd205e8356e89bf2032db522594a8924c68f
marentsov <mr.marentsov@gmail.com>
2025-03-14 09:53:27 +0300

    fix WF errors 4

--- page_analyzer/url_validator.py
+++ page_analyzer/url_validator.py
@@ -18,10 +18,10 @@ def normalize_url(url: str) -> str:
 def validate_url(url):
     errors = {}
 
-    if url == "":
-        errors['url'] = 'URL не может быть пустым'
     if not validators.url(url):
         errors['url'] = 'Некорректный формат URL'
+    if url == "":
+        errors['url'] = 'URL не может быть пустым'
     if len(url) > 255:
         errors['url'] = 'Слишком длинный URL (должен быть короче 255 символов)'
 
