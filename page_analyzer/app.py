f8db1fe15a212d1266d213154643211702b7d25b
marentsov <mr.marentsov@gmail.com>
2025-03-14 09:51:29 +0300

    add 500 error

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -103,3 +103,8 @@ def get_urls():
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('errors/404.html'), 404
+
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('errors/500.html'), 500
